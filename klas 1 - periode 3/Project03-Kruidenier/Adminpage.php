7b1d7a6e78936db70164656440a5c6b0c2d6c584
mark0042902 <markvandergraaf2004@gmail.com>
2021-04-12 09:42:54 +0200

    project

---
+++ klas 1 - periode 3/Project03-Kruidenier/Adminpage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <link rel="STYLESHEET" href="logincss.css" type="text/css">    
+
+        <title>Admin - De Kruidenier</title>
+    </head>
+    <body>
+        <div class="login">
+            <div class="txt">Welkom Admin,</div>
+            <div class="txt2">Klik hieronder op het gebied waar u graag heen wilt:</div><br>
+            <a href="login.php" class="button">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kassa&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><br>
+            <a href="login.php" class="button">&nbsp;&nbsp;Magazijn&nbsp;&nbsp;</a><br>
+            <a href="login.php" class="button">&nbsp;Gebruikers&nbsp;</a><br>
+            <a href="login.php" class="button">Beveiliging</a><br>
+            <a href="login.php" class="button">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home&nbsp;&nbsp;&nbsp;&nbsp;</a>
+        </div>
+    </body>
+</html>
---
+++ klas 1 - periode 3/Project03-Kruidenier/Login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <link rel="STYLESHEET" href="logincss.css" type="text/css">    
+
+        <title>Login - De Kruidenier</title>
+    </head>
+    <body>
+        <div class="login">
+            <div class="txt">Login</div>
+                <div class="loginplekken"> 
+                    Gebruikersnaam:<br>
+                    <input type="text" name="username" class="typvak"><br><br>
+                    Wachtwoord:<br>
+                    <input type="password" name="username" class="typvak"><br><br>
+                    <input type="submit" class="Loginbutton" value="Login">
+                </div>    
+        </div>
+    </body>
+</html>
