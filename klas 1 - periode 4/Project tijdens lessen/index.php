94cb0ec5a6731f01f756796e7b193ae550191cb6
mark0042902 <markvandergraaf2004@gmail.com>
2021-05-24 21:58:48 +0200

    lkdsjf

--- klas 1 - periode 4/Project tijdens lessen/index.php
+++ klas 1 - periode 4/Project tijdens lessen/index.php
@@ -1,10 +1,37 @@
-Getting Started With PHP</title>
+<html>
+
+<head>
+
+    <link rel="STYLESHEET" href="index.css" type="text/css">
+    <title>Homepage</title>
+</head>
+
 <body>
 
-<?php
-echo”Your first PHP code”;
-?>
+    <div class="sidebar">
+        <img class="logo" src="logo_vakantie.com.png"><br>
+        <a class="active" href="index.php">Home</a>
+        <a href="">Alle huisjes</a>
+        <a href="">Account</a>
+
+    </div>
+
+    <div id="start">
+        <h1 class="title">Goedendag, <br> klik op filteren om uw zoekresultaten te filteren</h1>
+    </div>
+    <button class="filter">
+        <label>Filteren</label>
+    </button>
+    <hr>
+
+    <div class="houses">
+        <ul class="pic">photo</ul>
+        <ul class="title">house</ul>
+        <ul class="desc">nice</ul>
+        <ul class="price">50</ul>
+    </div>
+
 
 </body>
+
 </html>
- 
