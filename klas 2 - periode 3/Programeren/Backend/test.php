0d2a05720edddc87cc6321ff33de6891eff53b83
mark0042902 <markvandergraaf2004@gmail.com>
2022-02-02 09:12:44 +0100

    sdf

---
+++ klas 2 - periode 3/Programeren/Backend/test.php
@@ -0,0 +1,27 @@
+<?php
+class Tree
+{
+    private string $leaves;
+    private string $branch;
+    private string $tribe;
+    private string $roots;
+
+    public function __construct($leaves, $branch, $tribe, $roots)
+    {
+        $this->$leaves = $leaves;
+        $this->$branch = $branch;
+        $this->$tribe = $tribe;
+        $this->$roots = $roots;
+    }
+
+    public function makeTree()
+    {
+        echo $this->leaves;
+        echo $this->branch;
+        echo $this->tribe;
+        echo $this->roots;
+
+    }
+}
+$tree = new Tree("oak", "oak", "oak", "oak");
+echo $this->makeTree;
